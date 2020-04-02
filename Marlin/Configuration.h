70eb31d2db59fc33eb94ccea9d47fbab65749bf2
Sergey <sergey@terentiev.me>
2020-04-02 20:02:35 +0300

    Шаги экструдера

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -743,7 +743,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 421 }
 
 /**
  * Default Max Feed Rate (mm/s)
