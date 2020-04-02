cca84156ced6b37311948b9777c1f331088da5a8
Sergey <sergey@terentiev.me>
2020-04-02 11:59:38 +0300

    Минимальная температура экструдера

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -571,7 +571,7 @@
  * *** IT IS HIGHLY RECOMMENDED TO LEAVE THIS OPTION ENABLED! ***
  */
 #define PREVENT_COLD_EXTRUSION
-#define EXTRUDE_MINTEMP 170
+#define EXTRUDE_MINTEMP 200
 
 /**
  * Prevent a single extrusion longer than EXTRUDE_MAXLENGTH.
