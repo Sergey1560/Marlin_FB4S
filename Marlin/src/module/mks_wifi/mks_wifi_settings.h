6668af3c8b7be70f507b71e15c2307ad5a5e6ce3
Sergey <sergey@terentiev.me>
2020-04-08 11:49:20 +0300

    Шаблон wifi настроек

--- Marlin/src/module/mks_wifi/mks_wifi_settings.h
+++ Marlin/src/module/mks_wifi/mks_wifi_settings.h
@@ -1,11 +1,7 @@
 #ifndef MKS_WIFI_SETTINGS_H
 #define MKS_WIFI_SETTINGS_H
 
-#define MKS_WIFI_SSID					"MikroTik2"
-#define MKS_WIFI_KEY					"vg3Apswqvg3Aps"
-
-//#define MKS_WIFI_SSID					"Dlink602_1"
-//#define MKS_WIFI_KEY					"Hnt45vh4JySqi"
-
+#define MKS_WIFI_SSID					"network"
+#define MKS_WIFI_KEY					"password"
 
 #endif
