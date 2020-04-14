a240f0ecc0a714c2edbbe8b1beb54d83813065a4
Sergey <sergey@terentiev.me>
2020-04-14 15:53:37 +0300

    В готовых сборках не была отключена конфигурация ESP

--- Marlin/src/module/mks_wifi/mks_wifi_settings.h
+++ Marlin/src/module/mks_wifi/mks_wifi_settings.h
@@ -1,7 +1,7 @@
 #ifndef MKS_WIFI_SETTINGS_H
 #define MKS_WIFI_SETTINGS_H
 
-#define MKS_WIFI_ENABLED_WIFI_CONFIG 
+//#define MKS_WIFI_ENABLED_WIFI_CONFIG 
 
 #define MKS_WIFI_SSID					"network"
 #define MKS_WIFI_KEY					"password"
