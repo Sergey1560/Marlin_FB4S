102a6fb2619a085057e3f2138e9d44fc422408a8
Sergey <sergey@terentiev.me>
2020-10-12 16:45:06 +0300

    Update to 2.0.7.2

--- Marlin/src/libs/W25Qxx.h
+++ Marlin/src/libs/W25Qxx.h
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Marlin 3D Printer Firmware
  * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
@@ -20,10 +20,6 @@
  *
  */
 #pragma once
-
-//#ifdef __cplusplus
-//extern "C" { /* C-declarations for C++ */
-//#endif
 
 #include <stdint.h>
 
