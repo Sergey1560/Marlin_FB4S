87b632dc0fd4e174d675c1d6b09ad03a9f6aa21c
X-Dron <X-Dron@mail.ru>
2020-12-29 17:55:24 +0300

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -749,7 +749,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {  160.0, 160.0, 400, 205.0 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {  160.5, 160.5, 400, 205.0 }
 
 /**
  * Default Max Feed Rate (mm/s)
