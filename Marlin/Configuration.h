1c7be914901a7511eac941a22421430341eae491
Sergey <sergey@terentiev.me>
2021-12-02 19:32:07 +0300

    Nano-s external driver direction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1362,7 +1362,11 @@
 #endif
 
 #ifdef FB_5_NANO_S_V1_3
-#define USR_E0_DIR false
+#ifdef EXT_EXTRUDER_DRIVER
+  #define USR_E0_DIR true
+#else
+  #define USR_E0_DIR false
+#endif
 #define USR_X_DIR true
 #define USR_Y_DIR true
 #define USR_Z_DIR false
